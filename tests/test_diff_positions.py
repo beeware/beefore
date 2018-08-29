0583185eb1e0f45979ad59845a5204551f512285
Russell Keith-Magee <russell@keith-magee.com>
2018-08-29 12:19:56 +1000

    Corrected diff position calculations.

--- tests/test_diff_positions.py
+++ tests/test_diff_positions.py
@@ -20,7 +20,7 @@ class TestDiff(unittest.TestCase):
         self.assertEqual(
             diff.positions('tests', diff_content),
             {
-                "path/to/testfile": {1: 3, 2: 4, 3: 5, 4: 6, 5: 7, 6: 8}
+                "path/to/testfile": {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6}
             }
         )
 
@@ -37,18 +37,21 @@ class TestDiff(unittest.TestCase):
         self.assertEqual(
             diff.positions('tests', diff_content),
             {
-                "path/to/testfile": {1: 3, 2: 6}
+                "path/to/testfile": {1: 1, 2: 4}
             }
         )
 
     def test_add_subtract(self):
         diff_content = [
             "diff --git a/tests/path/to/testfile b/tests/path/to/testfile",
-            " 1",
+            "index 5f4d692..5b05678 100644",
+            "--- a/tests/path/to/testfile",
+            "+++ b/tests/path/to/testfile",
             "@@ -2,0 +2,1 @@",
+            " 1",
             "+2",
             " 3",
-            "@@ -3,7 +4,4 @@",
+            "@@ -13,7 +14,4 @@",
             " 4",
             "-5",
             "-6",
@@ -61,7 +64,7 @@ class TestDiff(unittest.TestCase):
         self.assertEqual(
             diff.positions('tests', diff_content),
             {
-                "path/to/testfile": {2: 4, 3: 5, 4: 7, 5: 10, 6: 11, 7: 13}
+                "path/to/testfile": {2: 1, 3: 2, 4: 3, 14: 5, 15: 8, 16: 9, 17: 11}
             }
         )
 
