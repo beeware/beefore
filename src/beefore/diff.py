787cab1d30f23fd030513d1fa5c627749fd0aafd
Russell Keith-Magee <russell@keith-magee.com>
2018-08-29 12:46:23 +1000

    Corrected handling when there are multiple files.

--- src/beefore/diff.py
+++ src/beefore/diff.py
@@ -16,15 +16,18 @@ def positions(directory, diff_content):
     current_file = None
     file_start = None
     block_start = None
-    offset = None
+    block_offset = None
     for i, d in enumerate(diff_content):
         match = FILE_START.match(d)
         if match:
             current_file = {}
-
             filename = os.path.abspath(match.group(2))[len(directory)+1:]
             mappings[filename] = current_file
+
+            # Start of a new file; so reset the file andd block offsets.
             file_start = None
+            block_start = None
+            block_offset = None
         elif current_file is not None:
             match = LINE_RANGE.match(d)
             if match:
@@ -38,11 +41,11 @@ def positions(directory, diff_content):
                     file_start = i
 
                 block_start = i + 1
-                offset = 1 if match.group(1) == '0' else 0
+                block_offset = 1 if match.group(1) == '0' else 0
             elif block_start and len(d) > 0:
                 if d[0] == '-':
-                    offset += 1
+                    block_offset += 1
                 else:
-                    current_file[i - block_start - offset + first_line] = i - file_start
+                    current_file[i - block_start - block_offset + first_line] = i - file_start
 
     return mappings
--- tests/test_diff_positions.py
+++ tests/test_diff_positions.py
@@ -80,3 +80,47 @@ class TestDiff(unittest.TestCase):
             diff.positions('tests', diff_content),
             {}
         )
+
+    def test_multi_file(self):
+        diff_content = [
+            "diff --git a/tests/path/to/testfile b/tests/path/to/testfile",
+            "index 5f4d692..5b05678 100644",
+            "--- a/tests/path/to/testfile",
+            "+++ b/tests/path/to/testfile",
+            "@@ -2,0 +2,1 @@",
+            " 1",
+            "+2",
+            " 3",
+            "@@ -13,7 +14,4 @@",
+            " 4",
+            "-5",
+            "-6",
+            "+7",
+            " 8",
+            "-9",
+            "+10",
+            "diff --git a/tests/path/to/secondfile b/tests/path/to/secondfile",
+            "index 5f4d692..5b05678 100644",
+            "--- a/tests/path/to/secondfile",
+            "+++ b/tests/path/to/secondfile",
+            "@@ -2,0 +2,1 @@",
+            " 1",
+            "+2",
+            " 3",
+            "@@ -13,7 +14,4 @@",
+            " 4",
+            "-5",
+            "-6",
+            "+7",
+            " 8",
+            "-9",
+            "+10"
+        ]
+
+        self.assertEqual(
+            diff.positions('tests', diff_content),
+            {
+                "path/to/testfile": {2: 1, 3: 2, 4: 3, 14: 5, 15: 8, 16: 9, 17: 11},
+                "path/to/secondfile": {2: 1, 3: 2, 4: 3, 14: 5, 15: 8, 16: 9, 17: 11},
+            }
+        )
